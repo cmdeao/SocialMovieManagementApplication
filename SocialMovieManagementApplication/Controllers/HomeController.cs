538eb9d52f2c991575b597c727eb40744d4ee138
cmdeao <cmdeao16@gmail.com>
2021-07-05 18:58:19 -0500

    First iteration of movie searching operations with API.

--- SocialMovieManagementApplication/Controllers/HomeController.cs
+++ SocialMovieManagementApplication/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Web;
+using System.Net.Http;
 using System.Web.Mvc;
+using Newtonsoft.Json;
+using SocialMovieManagementApplication.Models;
 using SocialMovieManagementApplication.Services.Business;
+
 
 namespace SocialMovieManagementApplication.Controllers
 {
@@ -22,6 +24,9 @@ namespace SocialMovieManagementApplication.Controllers
                 ViewBag.Message = "Welcome to the application! Please login to fully" +
                     " utilize the applications features!";
             }
+
+            //await CheckAPIAsync();
+
             return View("Home");
         }
 
@@ -30,6 +35,43 @@ namespace SocialMovieManagementApplication.Controllers
             ViewBag.Message = "About the application!";
 
             return View();
+        }
+
+        public async System.Threading.Tasks.Task CheckAPIAsync()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://movie-database-imdb-alternative.p.rapidapi.com/?s=Avengers%20Endgame&page=1&r=json"),
+                Headers =
+                {
+                    { "x-rapidapi-key", "05daa4efc9msh2a725e479ff0086p11704ejsn648b6e1fc2ca" },
+                    { "x-rapidapi-host", "movie-database-imdb-alternative.p.rapidapi.com" },
+                },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+
+                //MovieModel model = JsonConvert.DeserializeObject<MovieModel>(body);
+
+                //Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(body);
+                //Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(body);
+                //Root myDeserializedClass = new Root();
+                //myDeserializedClass = JsonConvert.DeserializeObject<Root>(body);
+                //Debug.WriteLine("Count of list: " + myDeserializedClass.MovieSearchModel.Count);
+                //Debug.WriteLine("FOUND MOVIES:");
+                //for(int i = 0; i < myDeserializedClass.MovieSearchModel.Count; i++)
+                //{
+                //    Debug.WriteLine(myDeserializedClass.MovieSearchModel[i].Title);
+                //}
+
+                Debug.WriteLine("Old output!");
+                Debug.WriteLine(body);
+                //Console.WriteLine(body);
+            }
         }
 
         public ActionResult Contact()
@@ -54,4 +96,20 @@ namespace SocialMovieManagementApplication.Controllers
             return View("SearchResults", users);
         }
     }
+
+    //public class Search
+    //{
+    //    public string Title { get; set; }
+    //    public string Year { get; set; }
+    //    public string imdbID { get; set; }
+    //    public string Type { get; set; }
+    //    public string Poster { get; set; }
+    //}
+
+    //public class Root
+    //{
+    //    public List<Search> Search { get; set; }
+    //    public string totalResults { get; set; }
+    //    public string Response { get; set; }
+    //}
 }
